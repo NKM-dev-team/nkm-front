33a9f4c705a796e86e8a8db38f1f89b556d2b558
tojatos <tojatos@gmail.com>
2020-09-26 17:18:27 +0200

    Moved PropTypes

--- src/react-hexgrid/Hexagon/Text.js
+++ src/react-hexgrid/Hexagon/Text.js
@@ -1,27 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-// TODO Text is a separate component so that it could wrap the given text inside the surrounding hexagon
-class Text extends Component {
-  static propTypes = {
-    children: PropTypes.string,
-    x: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.number
-    ]),
-    y: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.number
-    ]),
-    className: PropTypes.string
-  };
+const Text = props => {
+  const { children, x, y, className } = props;
+  return (
+    <text x={x || 0} y={y ? y : '0.3em'} className={className} textAnchor="middle">{children}</text>
+  );
+};
 
-  render() {
-    const { children, x, y, className } = this.props;
-    return (
-      <text x={x || 0} y={y ? y : '0.3em'} className={className} textAnchor="middle">{children}</text>
-    );
-  }
-}
+Text.propTypes = {
+  children: PropTypes.string,
+  x: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number
+  ]),
+  y: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number
+  ]),
+  className: PropTypes.string
+};
 
 export default Text;
--- src/react-hexgrid/Pattern.js
+++ src/react-hexgrid/Pattern.js
@@ -1,29 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import HexUtils from './HexUtils';
 import Point from './models/Point';
 
-class Pattern extends Component {
-  static propTypes = {
-    id: PropTypes.string.isRequired,
-    link: PropTypes.string.isRequired,
-    size: PropTypes.object
-  };
-  static defaultProps = {
-    size: new Point(10, 10)
-  };
+const Pattern = props => {
+  const { id, link, size } = props;
 
-  render() {
-    const { id, link, size } = this.props;
+  return (
+    <defs>
+      <pattern id={id} patternUnits="objectBoundingBox" x={0} y={0} width={size.x} height={size.y}>
+        <image xlinkHref={link} x={0} y={0} width={size.x*2} height={size.y*2} />
+      </pattern>
+    </defs>
+  );
+};
 
-    return (
-      <defs>
-        <pattern id={id} patternUnits="objectBoundingBox" x={0} y={0} width={size.x} height={size.y}>
-          <image xlinkHref={link} x={0} y={0} width={size.x*2} height={size.y*2} />
-        </pattern>
-      </defs>
-    );
-  }
-}
+Pattern.propTypes = {
+  id: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+  size: PropTypes.object
+};
+
+Pattern.defaultProps = {
+  size: new Point(10, 10)
+};
 
 export default Pattern;
