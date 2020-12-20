209d0fdfe8610b12b2bfcae2cadca74f576bfb04
tojatos <tojatos@gmail.com>
2020-12-20 03:20:58 +0100

    added material-ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
+import { createMuiTheme, CssBaseline, ThemeProvider } from "@material-ui/core";
+
+const theme = createMuiTheme({
+  palette: {
+    type: "dark",
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
