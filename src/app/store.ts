4486f758d7b60ddcf0d7fb48427d5b7227175692
tojatos <tojatos@gmail.com>
2021-07-26 06:11:22 +0200

    persistence added

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,41 @@
-import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import {
+  configureStore,
+  ThunkAction,
+  Action,
+  getDefaultMiddleware,
+} from "@reduxjs/toolkit";
 import hexMapReducer from "../features/hexMapSlice";
 import authReducer from "../features/authSlice";
 import lobbiesReducer from "../features/lobbiesSlice";
+import storage from "redux-persist/lib/storage";
+import persistCombineReducers from "redux-persist/es/persistCombineReducers";
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage: storage,
+};
+const persistedRootReducer = persistCombineReducers(persistConfig, {
+  hexMapData: hexMapReducer,
+  authData: authReducer,
+  lobbiesData: lobbiesReducer,
+});
 
 export const store = configureStore({
-  reducer: {
-    hexMapData: hexMapReducer,
-    authData: authReducer,
-    lobbiesData: lobbiesReducer,
-  },
+  reducer: persistedRootReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER], // https://redux-toolkit.js.org/usage/usage-guide#use-with-redux-persist
+    },
+  }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
--- src/index.tsx
+++ src/index.tsx
@@ -9,20 +9,25 @@ import {
   CssBaseline,
   ThemeProvider,
 } from "@material-ui/core";
+import { PersistGate } from "redux-persist/integration/react";
+import persistStore from "redux-persist/es/persistStore";
 
 const theme = createMuiTheme({
   palette: {
     type: "dark",
   },
 });
+const persistor = persistStore(store);
 
 ReactDOM.render(
   // <React.StrictMode>
   <Provider store={store}>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Router />
-    </ThemeProvider>
+    <PersistGate persistor={persistor}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router />
+      </ThemeProvider>
+    </PersistGate>
   </Provider>,
   // </React.StrictMode>,
   document.getElementById("root")
