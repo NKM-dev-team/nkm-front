4f0136c75b9a67d28f6c6a6b49058369984761ed
tojatos <tojatos@gmail.com>
2023-01-07 10:41:52 +0100

    Status page improvements

--- src/views/StatusView.tsx
+++ src/views/StatusView.tsx
@@ -3,11 +3,15 @@ import {Box, Grid, Link, Paper, Typography} from "@mui/material";
 import { useSelector } from "react-redux";
 import { RootState } from "../app/store";
 import axios from "axios";
-import {GITHUB_SERVER_COMMIT_URL, GITHUB_SERVER_COMMITS_URL, GITHUB_SERVER_MASTER_HEAD_URL} from "../app/consts";
+import {
+  GITHUB_SERVER_COMMIT_URL,
+  GITHUB_SERVER_COMMITS_URL,
+  GITHUB_SERVER_MASTER_HEAD_URL,
+  VERSION_URL,
+} from "../app/consts";
 import {useMountEffect} from "../app/utils";
 
 export default function StatusView() {
-  const authData = useSelector((state: RootState) => state.authData);
   const versionData = useSelector((state: RootState) => state.versionData);
   const hexmapsData = useSelector((state: RootState) => state.hexMapData);
   const charactersData = useSelector((state: RootState) => state.charactersData);
@@ -15,6 +19,9 @@ export default function StatusView() {
   const lobbiesData = useSelector((state: RootState) => state.lobbiesData);
 
   const [githubServerVersion, setGithubServerVersion] = useState("unknown");
+  const [serverRunning, setServerRunning] = useState<boolean | undefined>(undefined);
+
+  const miniStatusXs = 6;
 
   useMountEffect(() => {
     axios.get(GITHUB_SERVER_MASTER_HEAD_URL)
@@ -22,35 +29,51 @@ export default function StatusView() {
         const version = res.data.object.sha;
         setGithubServerVersion(version);
       });
+
+    axios.get(VERSION_URL)
+      .then(res => {
+        setServerRunning(res.status == 200);
+      })
+      .catch(() => {
+        setServerRunning(false);
+      });
   });
 
 
   return (
     <Paper variant="outlined" sx={{p: 2, margin: 2}}>
-      <Grid container spacing={2} p={1}>
+      <Grid container justifyContent="center" spacing={2} p={1}>
         <Grid item xs={12}>
-          <Typography>
+          <Box sx={{
+            p: 2,
+            backgroundColor: serverRunning === undefined ? "yellow" : serverRunning ? "green" : "red",
+          }}>
+            Server status
+          </Box>
+        </Grid>
+        <Grid item xs={12}>
             {versionData.version ? (
               <>
+                <Typography>
                 Detected version on the server:
+                </Typography>
                 <Paper sx={{p: 2}}>
                   <Link target="_blank" href={GITHUB_SERVER_COMMIT_URL(githubServerVersion)}>{versionData.version}</Link>
                 </Paper>
               </>
             ): (
-              "Error with version detection."
+              <Typography>
+                "Error with version detection."
+              </Typography>
             )}
-          </Typography>
         </Grid>
         <Grid item xs={12}>
           <Typography>
-            <>
-              Latest version on github:
-              <Paper sx={{p: 2}}>
-                <Link target="_blank" href={GITHUB_SERVER_COMMIT_URL(githubServerVersion)}>{githubServerVersion}</Link>
-              </Paper>
-            </>
+            Latest version on github:
           </Typography>
+          <Paper sx={{p: 2}}>
+            <Link target="_blank" href={GITHUB_SERVER_COMMIT_URL(githubServerVersion)}>{githubServerVersion}</Link>
+          </Paper>
         </Grid>
         <Grid item xs={12}>
           <Typography>
@@ -59,7 +82,7 @@ export default function StatusView() {
             </>
           </Typography>
         </Grid>
-        <Grid item xs={4}>
+        <Grid item xs={miniStatusXs}>
           <Box sx={{
             p: 2,
             backgroundColor: hexmapsData.initialized ? "green" : "red",
@@ -67,7 +90,7 @@ export default function StatusView() {
             HexMaps metadata
           </Box>
         </Grid>
-        <Grid item xs={4}>
+        <Grid item xs={miniStatusXs}>
           <Box sx={{
             p: 2,
             backgroundColor: charactersData.initialized ? "green" : "red",
@@ -75,7 +98,7 @@ export default function StatusView() {
             Characters metadata
           </Box>
         </Grid>
-        <Grid item xs={4}>
+        <Grid item xs={miniStatusXs}>
           <Box sx={{
             p: 2,
             backgroundColor: abilitiesData.initialized ? "green" : "red",
@@ -83,7 +106,7 @@ export default function StatusView() {
             Abilities metadata
           </Box>
         </Grid>
-        <Grid item xs={4}>
+        <Grid item xs={miniStatusXs}>
           <Box sx={{
             p: 2,
             backgroundColor: lobbiesData.initialized ? "green" : "red",
