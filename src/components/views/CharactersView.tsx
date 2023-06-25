9063b764a6c74358e608f1bc09b297d1108c98b5
tojatos <tojatos@gmail.com>
2023-06-25 22:46:14 +0200

    NKM-93 Add characters search bar

--- src/components/views/CharactersView.tsx
+++ src/components/views/CharactersView.tsx
@@ -1,25 +1,55 @@
-import React from "react";
-import { Alert, Grid } from "@mui/material";
+import React, { useState } from "react";
+import { Alert, Autocomplete, Grid, TextField } from "@mui/material";
 import { useSelector } from "react-redux";
 import { RootState } from "../../app/store";
 import CharacterCard from "../characters_view/CharacterCard";
 
 export default function CharactersView() {
+  const [selectedCharacter, setSelectedCharacter] = useState<string | null>(
+    null
+  );
+
   const charactersData = useSelector(
     (state: RootState) => state.charactersData
   );
-  const characterSquares = [...charactersData.characterMetadataList]
-    .sort((a, b) => a.name.localeCompare(b.name))
-    .map((c) => (
-      <Grid item key={c.name}>
-        <CharacterCard c={c} />
-      </Grid>
-    ));
+
+  const characterMetadataListSorted = [
+    ...charactersData.characterMetadataList,
+  ].sort((a, b) => a.name.localeCompare(b.name));
+
+  const characterMetadataListFiltered =
+    selectedCharacter !== null
+      ? characterMetadataListSorted.filter((c) => c.name === selectedCharacter)
+      : characterMetadataListSorted;
+
+  const characterSquares = characterMetadataListFiltered.map((c) => (
+    <Grid item key={c.name}>
+      <CharacterCard c={c} />
+    </Grid>
+  ));
+
+  const handleCharacterNameAutocompleteChange = (
+    event: React.ChangeEvent<{}>,
+    value: string | null
+  ) => {
+    setSelectedCharacter(value);
+  };
 
   return charactersData.initialized ? (
-    <Grid container justifyContent="center" spacing={3} p={2}>
-      {characterSquares}
-    </Grid>
+    <>
+      <Autocomplete
+        options={characterMetadataListSorted.map((c) => c.name)}
+        value={selectedCharacter}
+        onChange={handleCharacterNameAutocompleteChange}
+        renderInput={(params) => (
+          <TextField {...params} label="SEARCH" variant="outlined" />
+        )}
+        sx={{ p: 2 }}
+      />
+      <Grid container justifyContent="center" spacing={3} p={2}>
+        {characterSquares}
+      </Grid>
+    </>
   ) : (
     <Alert severity="error">
       Character data is not initialized, please check your internet connection.
