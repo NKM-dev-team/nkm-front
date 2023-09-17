13fbcb8acb345c5fd618f0ddac7b6ad9b67fc74e
tojatos <tojatos@gmail.com>
2023-09-18 01:20:45 +0200

    NKM-236 Unity improvements such as credential injection

--- src/components/UnityBuild.tsx
+++ src/components/UnityBuild.tsx
@@ -1,23 +1,48 @@
-import { Box, Button } from "@mui/material";
-import React, { useEffect } from "react";
+import { Box, Button, Grid } from "@mui/material";
+import React, { useState } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
+import { useSelector } from "react-redux";
+import { RootState } from "../app/store";
+import { AuthResponse } from "../types/AuthResponse";
 
 export default function UnityBuild() {
   const buildFolder = "unity_build";
   const buildName = "nkm-webgl-build";
 
-  const { unityProvider, requestFullscreen, unload } = useUnityContext({
-    loaderUrl: `${buildFolder}/${buildName}.loader.js`,
-    dataUrl: `${buildFolder}/${buildName}.data`,
-    frameworkUrl: `${buildFolder}/${buildName}.framework.js`,
-    codeUrl: `${buildFolder}/${buildName}.wasm`,
-  });
+  const { unityProvider, requestFullscreen, unload, sendMessage, isLoaded } =
+    useUnityContext({
+      loaderUrl: `${buildFolder}/${buildName}.loader.js`,
+      dataUrl: `${buildFolder}/${buildName}.data`,
+      frameworkUrl: `${buildFolder}/${buildName}.framework.js`,
+      codeUrl: `${buildFolder}/${buildName}.wasm`,
+    });
 
-  useEffect(() => {
-    return () => {
-      unload();
+  const authData = useSelector((state: RootState) => state.authData);
+  const [isUnityStopped, setIsUnityStopped] = useState<boolean>(false);
+
+  const generateKey = (): string => new Date().getTime().toString();
+  const [unityKey, setUnityKey] = useState<string>(generateKey());
+
+  const canInjectCredentials = !!authData.token && !!authData.userState;
+
+  const injectCredentials = () => {
+    if (!authData.token || !authData.userState) return;
+
+    let resp: AuthResponse = {
+      token: authData.token,
+      userState: authData.userState,
     };
-  }, []);
+    sendMessage("MenuManager", "WebglLogin", JSON.stringify(resp));
+  };
+
+  const quitUnity = async () => {
+    unload().then(() => setIsUnityStopped(true));
+  };
+
+  const startUnity = async () => {
+    setUnityKey(generateKey());
+    setIsUnityStopped(false);
+  };
 
   return (
     <>
@@ -25,11 +50,47 @@ export default function UnityBuild() {
         <Unity
           unityProvider={unityProvider}
           style={{ width: 1024, height: 576 }}
+          key={unityKey}
         />
         <Box textAlign="center">
-          <Button variant="contained" onClick={() => requestFullscreen(true)}>
-            Fullscreen
-          </Button>
+          <Grid container flexDirection="column" spacing={1}>
+            <Grid item>
+              <Button
+                variant="contained"
+                onClick={startUnity}
+                disabled={!isUnityStopped}
+              >
+                Start
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button
+                variant="contained"
+                onClick={() => requestFullscreen(true)}
+                disabled={isUnityStopped}
+              >
+                Fullscreen
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button
+                variant="contained"
+                onClick={injectCredentials}
+                disabled={isUnityStopped || !canInjectCredentials}
+              >
+                Inject credentials
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button
+                variant="contained"
+                onClick={quitUnity}
+                disabled={isUnityStopped}
+              >
+                Quit
+              </Button>
+            </Grid>
+          </Grid>
         </Box>
       </Box>
     </>
