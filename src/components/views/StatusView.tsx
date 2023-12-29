180af8ea3e7e4428f8184981b45ac622e28f2ff6
tojatos <tojatos@gmail.com>
2023-12-29 21:14:20 +0100

    Add health check section to status view

--- src/components/views/StatusView.tsx
+++ src/components/views/StatusView.tsx
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Grid, Link, Paper, Typography } from "@mui/material";
 import { useSelector } from "react-redux";
 import { RootState } from "../../app/store";
 import axios from "axios";
 import {
+  GET_GAME_STATE_URL,
+  GET_LOBBIES_URL,
+  GET_LOBBY_URL,
   GITHUB_SERVER_COMMIT_URL,
   GITHUB_SERVER_COMMITS_URL,
   GITHUB_SERVER_MASTER_HEAD_URL,
@@ -11,6 +14,27 @@ import {
   VERSION_URL_STABLE,
 } from "../../app/consts";
 import { useMountEffect } from "../../app/utils";
+import { LobbyState } from "../../types/lobby/LobbyState";
+
+interface HealthCheckBoxProps {
+  label: string;
+  backgroundColor: string;
+}
+
+function HealthCheckBox({ label, backgroundColor }: HealthCheckBoxProps) {
+  return (
+    <Grid item md={3} xs={6}>
+      <Box
+        sx={{
+          p: 2,
+          backgroundColor: backgroundColor,
+        }}
+      >
+        {label}
+      </Box>
+    </Grid>
+  );
+}
 
 export default function StatusView() {
   const versionData = useSelector((state: RootState) => state.versionData);
@@ -22,8 +46,62 @@ export default function StatusView() {
   const [serverRunning, setServerRunning] = useState<boolean | undefined>(
     undefined
   );
+  const [getLobbiesRunning, setGetLobbiesRunning] = useState<
+    boolean | undefined
+  >(undefined);
+
+  const [someGameId, setSomeGameId] = useState<string | undefined>(undefined);
+
+  const [getLobbyStateRunning, setGetLobbyStateRunning] = useState<
+    boolean | undefined
+  >(undefined);
+
+  const [getGameStateRunning, setGetGameStateRunning] = useState<
+    boolean | undefined
+  >(undefined);
+
+  useEffect(() => {
+    if (someGameId === undefined) return;
+    axios
+      .get(GET_LOBBY_URL(someGameId))
+      .then((res) => {
+        if (res.status === 200) {
+          setGetLobbyStateRunning(true);
+        }
+      })
+      .catch(() => {
+        setGetLobbyStateRunning(false);
+      });
+
+    axios
+      .get(GET_GAME_STATE_URL(someGameId))
+      .then((res) => {
+        if (res.status === 200) {
+          setGetGameStateRunning(true);
+        }
+      })
+      .catch(() => {
+        setGetGameStateRunning(false);
+      });
+  }, [someGameId]);
 
   useMountEffect(() => {
+    axios
+      .get(GET_LOBBIES_URL)
+      .then((res) => {
+        if (res.status === 200) {
+          const lobbies: LobbyState[] = res.data;
+          setGetLobbiesRunning(true);
+          if (lobbies) {
+            setSomeGameId(lobbies[0].id);
+          }
+        }
+      })
+      .catch((e) => {
+        console.warn(e);
+        setGetLobbiesRunning(false);
+      });
+
     axios.get(GITHUB_SERVER_MASTER_HEAD_URL).then((res) => {
       const version = res.data.object.sha;
       setGithubServerVersion(version);
@@ -39,8 +117,8 @@ export default function StatusView() {
           console.error("stable version undefined");
         }
       })
-      .catch(() => {
-        console.error("stable version undefined");
+      .catch((e) => {
+        console.warn(e);
       });
 
     axios
@@ -48,28 +126,44 @@ export default function StatusView() {
       .then((res) => {
         setServerRunning(res.status === 200 && res.data.length === 40);
       })
-      .catch(() => {
+      .catch((e) => {
+        console.warn(e);
         setServerRunning(false);
       });
   });
 
+  const statusToColor = (status: boolean | undefined) => {
+    switch (status) {
+      case true:
+        return "green";
+      case false:
+        return "red";
+      case undefined:
+        return "yellow";
+    }
+  };
+
   return (
     <Paper variant="outlined" sx={{ p: 2, margin: 2 }}>
       <Grid container justifyContent="center" spacing={2} p={1}>
-        <Grid item xs={12}>
-          <Box
-            sx={{
-              p: 2,
-              backgroundColor:
-                serverRunning === undefined
-                  ? "yellow"
-                  : serverRunning
-                  ? "green"
-                  : "red",
-            }}
-          >
-            Server status
-          </Box>
+        <Typography>Health check:</Typography>
+        <Grid container justifyContent="center" spacing={2} p={1}>
+          <HealthCheckBox
+            label="Server status (latest)"
+            backgroundColor={statusToColor(serverRunning)}
+          />
+          <HealthCheckBox
+            label="Lobbies"
+            backgroundColor={statusToColor(getLobbiesRunning)}
+          />
+          <HealthCheckBox
+            label="Lobby State"
+            backgroundColor={statusToColor(getLobbyStateRunning)}
+          />
+          <HealthCheckBox
+            label="Game State"
+            backgroundColor={statusToColor(getGameStateRunning)}
+          />
         </Grid>
         <Grid item xs={12}>
           {versionData.version ? (
